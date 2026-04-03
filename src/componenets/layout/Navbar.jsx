5ec3d0e401f31cc28c34053a67c902a72b756c3b
Masud <wubetumasud@gmail.com>
2026-04-04 00:51:21 +0300

    build navbar

--- src/componenets/layout/Navbar.jsx
+++ src/componenets/layout/Navbar.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Code, Menu, X } from 'lucide-react'
+import { navLinks, personalInfo } from '../../utils/constants'
+import useScrollSpy from '../../hooks/useScrollSpy'
+import { useState } from 'react'
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuopen] = useState(false);
+  const [ isScrolled, setIsScrolled ] = useState(false);
+  const activeSection = useScrollSpy(navLinks.map(link => link.id));
+
+  useEffect
+
+
   return (
     <div>Navbar</div>
   )
